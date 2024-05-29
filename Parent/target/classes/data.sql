2e525c217410d582e2dcdb15d47fa1002296e5d4
ricardo-fonseca01 <102249643+ricardo-fonseca01@users.noreply.github.com>
2024-05-29 16:58:21 +0100

    target_parent

---
+++ Parent/target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO parents (egn, age, email, first_name, gender, last_name, middle_name, phone_number)
+VALUES (12345, 50, 'maria@gmail.com', 'Maria', 'F', 'Aldina', 'Curtina', '923567891'),
+       (123456, 52, 'manuel@gmail.com', 'Manuel', 'M', 'Aldina', 'Curtina', '965718235');
+
